03029e6fa42ddaa9ac8716a867f7c4ac6ffc77cb
Jonathan Morales Lopez <74636947+JonathanML203@users.noreply.github.com>
2024-06-04 12:16:06 -0600

    Add files via upload

---
+++ ParImpar/ParImpar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Por favor, introduce un número:");
+            string input = Console.ReadLine();
+            int numero;
+            if (int.TryParse(input, out numero))
+            {
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine("El número " + numero + " es par.");
+                }
+                else
+                {
+                    Console.WriteLine("El número " + numero + " es impar.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrada no válida. Por favor, introduce un número entero.");
+            }
+        }
+    }
+}
